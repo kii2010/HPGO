2939a901d4e550ef0694461fde26806cb9d3ca5d
정재욱 <j3j3y123@naver.com>
2018-09-19 02:15:39 +0900

    메인화면 레이아웃 완성

--- app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/MainActivity.java
+++ app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/MainActivity.java
@@ -3,11 +3,11 @@ package actionsiteidhampyeong_hs.kruserindexmain.jne.hs.httphampyeong.hpgo1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity1 extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main1);
+        setContentView(R.layout.activity_main);
     }
 }
