7f02baa5f9e0b9bceb24821ff1d80bb11e67390d
정재욱 <j3j3y123@naver.com>
2018-09-19 22:39:23 +0900

    변수 및 ID 정리

--- app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/MainActivity.java
+++ app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/MainActivity.java
@@ -5,13 +5,12 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout linearLayout;
+    LinearLayout school_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +18,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //버튼 검색
-        linearLayout = findViewById(R.id.main_top_text);
+        school_text = findViewById(R.id.main_top_text);
 
-        linearLayout.setOnClickListener(new View.OnClickListener() {
+        //홈페이지 이동 구현
+        school_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri gotoschoolpage = Uri.parse("http://hampyeong.hs.jne.kr/user/hampyeong_hs/index.action");
-                Intent goschoolpage = new Intent(Intent.ACTION_VIEW, gotoschoolpage);
-                startActivity(goschoolpage);
+                Uri go_to_schoolpage = Uri.parse("http://hampyeong.hs.jne.kr/user/hampyeong_hs/index.action");
+                Intent go_schoolpage = new Intent(Intent.ACTION_VIEW, go_to_schoolpage);
+                startActivity(go_schoolpage);
+                Toast.makeText(getApplicationContext(),"홈페이지로 이동중", Toast.LENGTH_SHORT).show();
             }
         });
     }
