cafee23d105f72af0ddcc25cc4606fbaf804ad59
TW <kii2010@naver.com>
2018-09-25 23:08:21 +0900

    빨간맛

--- app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/noticeActivity.java
+++ app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/noticeActivity.java
@@ -1,13 +1,24 @@
 package actionsiteidhampyeong_hs.kruserindexmain.jne.hs.httphampyeong.hpgo1;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
-public class noticeActivity extends AppCompatActivity {
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class noticeActivity extends AppCompatActivity {
+    private NotiAdapter mNotiAdaper;
+    private ArrayList<NotiItem> mNotiList;
     Toolbar noti_toolbar;
     RecyclerView noti_recycler;
 
@@ -22,6 +33,53 @@ public class noticeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         noti_recycler.setLayoutManager(new LinearLayoutManager(this));
 
+    }
+
+    private class Notiparse extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog asyncDialog = new ProgressDialog(noticeActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            asyncDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            asyncDialog.setMessage("공지사항를 불러오는 중입니다.");
+
+            // show dialog
+            asyncDialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                Document noti_doc = Jsoup.connect("http://hampyeong.hs.jne.kr/user/indexSub.action?framePath=unknownboard&siteId=hampyeong_hs&dum=dum&boardId=422382&page=1&command=list&status=").get();
+                Element table = noti_doc.select("table.wb tbody").get(0);
+                Elements rows = table.select("tr");
+
+                for (int i = 1; i < rows.size(); i++) {
+                    Element row = rows.get(i);
+                    Element title = row.select("td").get(1);
+                    Element writer = row.select("td").get(2);
+                    Element date = row.select("td").get(4);
+
+                    String notiTitle = title.text();
+                    String notiWriter = writer.text();
+                    String notiDate = date.text();
+
+                    mNotiList.add(new NotiItem(notiTitle, notiDate, notiWriter));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            mNotiAdaper = new NotiAdapter(noticeActivity.this, mNotiList);
+            noti_recycler.setAdapter(mNotiAdaper);
+            asyncDialog.dismiss();
+        }
     }
 }
 
