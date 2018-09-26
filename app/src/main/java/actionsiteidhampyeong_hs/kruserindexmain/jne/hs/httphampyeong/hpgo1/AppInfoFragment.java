3a7da7bee0bc7e02696d8f4774b982a0ca4d13cd
TW <kii2010@naver.com>
2018-09-26 21:12:19 +0900

    앱정보 구현중입니다.

---
+++ app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/AppInfoFragment.java
@@ -0,0 +1,16 @@
+package actionsiteidhampyeong_hs.kruserindexmain.jne.hs.httphampyeong.hpgo1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class AppInfoFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings_preference)
+
+
+    }
+}
