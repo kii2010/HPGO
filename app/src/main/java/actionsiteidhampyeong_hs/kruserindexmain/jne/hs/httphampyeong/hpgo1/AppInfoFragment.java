aa892e1a47c3fd767b5161c8f0467227c2a2a94a
정재욱 <j3j3y123@naver.com>
2018-09-26 21:20:28 +0900

    앱정보 구현 완료

--- app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/AppInfoFragment.java
+++ app/src/main/java/actionsiteidhampyeong_hs/kruserindexmain/jne/hs/httphampyeong/hpgo1/AppInfoFragment.java
@@ -1,16 +1,16 @@
 package actionsiteidhampyeong_hs.kruserindexmain.jne.hs.httphampyeong.hpgo1;
 
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class AppInfoFragment extends AppCompatActivity {
+public class AppInfoFragment extends PreferenceFragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.settings_preference)
-
-
+        addPreferencesFromResource(R.xml.settings_preference);
     }
 }
